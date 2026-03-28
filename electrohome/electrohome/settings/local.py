bbf4998b58b44dd1a802baf1703ee998eacf0428
alan coneo rodriguez <alanconeorodriguez1130@gmail.com>
2026-03-28 15:00:34 -0500

    actualize una linea en base para la producion del ecomerce

--- electrohome/electrohome/settings/local.py
+++ electrohome/electrohome/settings/local.py
@@ -10,18 +10,12 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 # ===== CONFIGURACIÓN DE BASE DE DATOS POSTGRESQL =====
 import dj_database_url
 
-# DATABASES = {                          # <- comenta esto
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
 # ===== CONFIGURACIÓN DE CACHÉ =====
 CACHES = {
     'default': {
